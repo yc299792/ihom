f60109606f7299a7e34120605425b1181ef8efaa
yc299792 <1776216270@qq.com>
2019-11-07 08:45:24 +0800

    个人信息功能完成，包括修改，以及实名认证等功能

--- ihome/utils/image_storage.py
+++ ihome/utils/image_storage.py
@@ -9,8 +9,8 @@ from qiniu import Auth, put_data, etag, urlsafe_base64_encode
 import qiniu.config
 
 # 需要填写你的 Access Key 和 Secret Key
-access_key = 'uzc59bVURbUbazey9vrexXKocNKBUN8NuLijk57N'
-secret_key = '-9lenw28jU2REojvGkcsEPWk5Nm9V2HIVqb5Nkts'
+access_key = 'XbMtctkseWYatm-nqukiwRfhcCi8o5bf75J3uoRF'
+secret_key = 'BdMUOiztN6-kSuVXUT29NQwWKMFpQudEQR3An32C'
 
 
 def storage(file_data):
@@ -23,7 +23,7 @@ def storage(file_data):
     q = Auth(access_key, secret_key)
 
     # 要上传的空间
-    bucket_name = 'ihome'
+    bucket_name = 'ihome-yc'
 
     # 生成上传 Token，可以指定过期时间等
     token = q.upload_token(bucket_name, None, 3600)
